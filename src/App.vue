7653b4b462f2789ad678f4f1d07bbfc2b93b3fbc
hee14780 <hee14780@gmail.com>
2019-09-30 17:53:23 +0900

    class명 변경

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,32 @@
 <template>
-    <div class="ramaya-container">
-        <Menu/>
-        <router-view class="ramaya-contents"/>
+    <div class="goodPlace-container">
+        <Menu />
+        <router-view class="goodPlace-contents" />
     </div>
 </template>
 
 <script>
-    import './mixins/common-mixin.js'
-    import './mixins/Http.js'
-    import './assets/js/console.js'
-    import Vue from 'vue'
-    import store from './vuex/store'
-    import 'babel-polyfill'
+import "./mixins/common-mixin.js";
+import "./mixins/Http.js";
+import "./assets/js/console.js";
+import Vue from "vue";
+import store from "./vuex/store";
+import "babel-polyfill";
 
-    import VueClipboard from 'vue-clipboard2'
-    VueClipboard.config.autoSetContainer = true;
-    Vue.use(VueClipboard);
+import VueClipboard from "vue-clipboard2";
+VueClipboard.config.autoSetContainer = true;
+Vue.use(VueClipboard);
 
-    import Menu from './components/menu'
+import Menu from "./components/menu";
 
-    export default {
-        components: {Menu},
+export default {
+    components: { Menu },
 
-        store,
-        name : 'App'
-    }
+    store,
+    name: "App"
+};
 </script>
 
 <style scoped>
-    /*.content-wrap { padding: 20px }*/
+/*.content-wrap { padding: 20px }*/
 </style>
